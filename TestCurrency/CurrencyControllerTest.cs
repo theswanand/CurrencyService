144d0291cd5b274e81e00dbbf347317c2492ce34
theswanand <swanand.pangam@gmail.com>
2025-05-19 11:37:00 +0530

    Updated test cases

--- TestCurrency/CurrencyControllerTest.cs
+++ TestCurrency/CurrencyControllerTest.cs
@@ -32,18 +32,13 @@ namespace TestCurrency
         {
             // Arrange
             var options = new DbContextOptionsBuilder<CurrencyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .UseSqlServer(connectionString: "Server=localhost;Database=Currency;Trusted_Connection=True;TrustServerCertificate=True;")
                 .Options;
-            using (var context = new CurrencyDbContext(options))
-            {
-                context.Currencies.Add(new Currency { Code = "USD", Rate = 6.5m, LastUpdated = DateTime.UtcNow });
-                context.SaveChanges();
-            }
             using (var context = new CurrencyDbContext(options))
             {
                 var controller = new CurrencyController(context);
                 // Act
-                var result = controller.GetLatestDanishKronesRate("INR") as NotFoundObjectResult;
+                var result = controller.GetLatestDanishKronesRate("AAA") as NotFoundObjectResult;
                 // Assert
                 Assert.AreEqual(404, result.StatusCode);
             }
